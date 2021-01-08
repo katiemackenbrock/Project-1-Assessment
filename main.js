0f70a16adf3b4beb558f6197a4df480af370dd0d
katiemackenbrock <katiemackenbrock@gmail.com>
2021-01-07 18:22:47 -0800

    erik tutorial saved all

--- main.js
+++ main.js
@@ -27,15 +27,37 @@
 
 
 //node.textContent source https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
-var number = 0;
-var span = document.querySelector('span');
-var plus = document.getElementById('plus');
-var minus = document.getElementById('minus');
+// var number = 0;
+// var span = document.querySelector('span');
+// var plus = document.getElementById('plus');
+// var minus = document.getElementById('minus');
 
-plus.addEventListener('click', function () {
-    span.textContent = number++;
-});
+// plus.addEventListener('click', function () {
+//     span.textContent = number++;
+// });
 
-minus.addEventListener('click', function (){
-    span.textContent = number--;
-});
+// minus.addEventListener('click', function (){
+//     span.textContent = number--;
+// });
+
+const display = document.getElementById("display");
+const input = document.getElementById("num-input");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+let num = 0;
+
+function addNum(e) {
+    e.preventDefault();
+    num += parseInt(input.value);
+    display.innerText = num;
+}
+
+function subtractNum(e) {
+    e.preventDefault();
+    num -= parseInt(input.value);
+    display.innerText = num;
+}
+
+plus.addEventListener("click", (e)=> addNum(e))
+minus.addEventListener("click", (e)=> subtractNum(e))
